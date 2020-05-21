8a72c267a6c0260b5c203c7db074a78e0fe53c40
KyungJae Min <kjm537@nyu.edu>
2020-05-21 22:48:09 +0900

    is palindrome

---
+++ ez/is-palindrome.py
@@ -0,0 +1,18 @@
+# attempt
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        str_x = str(x)
+        left = 0
+        right = len(str_x) - 1
+        while left <= right:
+            if str_x[left] != str_x[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+# optimal
+return False if x < 0 else x == int(str(x)[::-1])
+
+# this is actually slower because it has to go through the whole string but thought it was a clever one liner
+# made use string slicing
